0f14cc49d91b42862f334ba3526f6d20394903d0
FrancoTLopez <francotomaslopez158@gmail.com>
2025-04-03 19:07:47 +0200

    Add files via upload

---
+++ proyecto-tomas/js/index.js
@@ -0,0 +1,19 @@
+function redirection1(){
+    location.href="index.html"
+}
+function redirection2(){
+    location.href="pag2.html"
+}
+function redirection3(){
+    location.href="pag3.html"
+}
+function redirection4(){
+    location.href="pag4.html"
+}
+function redirection5(){
+    location.href="pag5.html"
+}
+
+
+
+
